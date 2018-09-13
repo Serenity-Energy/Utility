64fff8739c0d80d216f24fcbe7e0940bf05efe08
serenitysource <42885275+serenitysource@users.noreply.github.com>
2018-09-13 16:00:49 +1000

    Update CarbonToken.sol

--- contracts/CarbonToken.sol
+++ contracts/CarbonToken.sol
@@ -45,7 +45,7 @@ contract TokenERC20 {
     string public name;
     string public symbol;
     uint8 public decimals = 18;
-    // 18 decimals is the strongly suggested default, avoid changing it
+    // 18 decimals is the strongly suggested default, avoid changing it!
     uint256 public totalSupply;
 
     // This creates an array with all balances
