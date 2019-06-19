97e5de015dbe44eed2313820cc9af7007c0e2fb4
serenitysource <42885275+serenitysource@users.noreply.github.com>
2019-06-19 21:16:55 +1000

    Update CarbonToken.sol

--- contracts/CarbonToken.sol
+++ contracts/CarbonToken.sol
@@ -264,7 +264,7 @@ contract CarbonToken is owned, TokenERC20 {
     /// @param amount amount of tokens to be sold
     function sell(uint256 amount) public {
         address myAddress = this;
-        require(myAddress.balance >= amount * sellPrice);      // checks if the contract has enough ether to buy
+        require(myAddress.balance >= amount * sellPrice); // checks if the contract has enough ether to buy
         _transfer(msg.sender, this, amount);              // makes the transfers
         msg.sender.transfer(amount * sellPrice);          // sends ether to the seller. It's important to do this last to avoid recursion attacks
     }
